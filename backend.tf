51090ec3874fd9e715508c91c1aa25caf1322d78
bus57790 <30909202+bus57790@users.noreply.github.com>
2026-02-10 22:02:18 -0500

    Update S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "infra/terraformstatefile"
-    bucket         = "betech-terraform-state-bucket-2025"
+    bucket         = "betech-terraform-state-bucket-2027"
     region         = "us-west-2"
     dynamodb_table = "betech-terraform-locks"
   }
